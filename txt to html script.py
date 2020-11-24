d56b7e1380f703f05ee2bf3d23c2a5be22cd5ee2
Evan Thomas <74596543+EvanDanielThomas@users.noreply.github.com>
2020-11-24 07:13:29 -0500

    Update txt to html script.py

--- txt to html script.py
+++ txt to html script.py
@@ -1,7 +1,21 @@
 import tkinter as tk #pyperclip module necessary for copy/paste function
 
+finalcode = "<!DOCTYPE html>\n<html>\n"
+
+finalcode = finalcode + "<header>\n"
+title = input("What would you like the title of your webpage to be?   This is the text that shows at the top of the tab when you open it.  ")
+titleconfirmation = "N"
+while titleconfirmation != "Y":
+    titleconfirmation = input("Are you sure you want " + title + " to be your heading? (Y/N): ")
+    if titleconfirmation == "N":
+        title = input("What would you like the title of your webpage to be?   This is the text that shows at the top of the tab when you open it.  ")
+
+finalcode = finalcode + "<title>" + title + "</title>\n</header>\n"
+
+
+finalcode = finalcode + "<body>\n"
 #Main Heading
-heading = input("What would you like the heading of your website to be? ")
+heading = input("What would you like the heading of your website to be?  This will the the large text displayed at the top of your webpage.  ")
 headingconfirmation = False
 while headingconfirmation != True:
     headingconfirmation = input("Are you fine with " + heading + " as your heading?  (Y/N): ")
@@ -10,13 +24,13 @@ while headingconfirmation != True:
     elif headingconfirmation == "N":
         heading = input("What would you like the heading of your website to be?  ")
 
-finalcode = ("<h1>" + heading + "</h1>" + "\n")
+finalcode = finalcode + ("<h1>" + heading + "</h1>" + "\n")
 x = 1
 
 while x == 1:
     elementconfirmed = False
     while elementconfirmed != True:
-        element = input("What would you like next in your website?  A sub-heading or a paragraph? (H/P): ")
+        element = input("What would you like next in your website?  A sub-heading, a paragraph, or a list? (H/P/L): ")
         if element == "P":
             element = "paragraph"
             content = input("What would you like your paragraph to say?  Please only insert one paragraph. ")
@@ -57,9 +71,37 @@ while x == 1:
                         break
                 elif confirmation == "N":
                     content = input("What would you like your sub-heading to say? Only one line of text is supported. ")
+        elif element == "L":
+            listtype = input("Would you like a bulleted or numbered list? (B/N): ")
+            if listtype == "B":
+                finalcode = finalcode + "<ul>\n"
+                content = " "
+                while listtype == "B" and content != "*ABORT*":
+                    content = input("What would you like this bullet to say?  Type *ABORT* if you'd like to stop adding bullets. ")
+                    if content == "*ABORT*":
+                        finalcode = finalcode + "</ul>\n"
+                        print("Here's your code so far: " + finalcode)
+                        break
+                    finalcode = finalcode + "\t<li>" + content + "</li>\n"
+            if listtype == "N":
+                finalcode = finalcode + "<ol>\n"
+                content = " "
+                while listtype == "N" and content != "*ABORT*":
+                    content = input("What would you like this bullet to say?  Type *ABORT* if you'd like to stop adding bullets. ")
+                    if content == "*ABORT*":
+                        finalcode = finalcode + "</ol>\n"
+                        break
+                    finalcode = finalcode + "\t<li>" + content + "</li>\n"
+                
+finalcode = finalcode + "</body>\n</html>"
 
 root = tk.Tk()
-root.withdraw #(prevents the tkinter window from showing)
+#root.withdraw #(prevents the tkinter window from showing)
 root.clipboard_append(finalcode)
-print("Your code should have copied automatically.  If it did not, please copy it manually below:")
+
+outF = open("code.html", "w")
+outF.writelines(finalcode)
+outF.close()
+
+print("Your code should also have also copied to your clipboard automatically.  If it did not, please copy it manually below:")
 print(finalcode)
